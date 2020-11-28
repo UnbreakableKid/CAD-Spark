f5a9382709b7c32beeb06775dfaece166ea590f4
João Rafael Pacheco <dblamk@hotmail.com>
2020-11-28 17:56:12 +0000

    route with more done

---
+++ src/main/java/cadlabs/rdd/AverageDurationFlightsPerRoute.java
@@ -0,0 +1,24 @@
+package cadlabs.rdd;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import scala.Tuple2;
+
+import java.util.Map;
+
+public class AverageDurationFlightsPerRoute extends AbstractFlightAnalyser<Map<Tuple2<String, String>, Long>> {
+
+
+	public AverageDurationFlightsPerRoute(JavaRDD<Flight> flights) {
+		super(flights);
+	}
+	
+	
+	public Map<Tuple2<String, String>, Long> run() {
+		JavaPairRDD<Tuple2<String, String>, Long> allRoutes =
+				this.flights.mapToPair(flight -> new Tuple2<>(new Tuple2<>(flight.origin, flight.dest) , (long)1));
+
+		return allRoutes.countByKey();
+	}
+
+}
--- src/main/java/cadlabs/rdd/RouteWithMoreFlights.java
+++ src/main/java/cadlabs/rdd/RouteWithMoreFlights.java
@@ -4,9 +4,10 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import scala.Tuple2;
 
+import java.util.List;
 import java.util.Map;
 
-public class RouteWithMoreFlights extends AbstractFlightAnalyser<Map<Tuple2<String, String>, Long>> {
+public class RouteWithMoreFlights extends AbstractFlightAnalyser<Tuple2<Tuple2<String, String>, Long>> {
 
 
 	public RouteWithMoreFlights(JavaRDD<Flight> flights) {
@@ -14,15 +15,19 @@ public class RouteWithMoreFlights extends AbstractFlightAnalyser<Map<Tuple2<Stri
 	}
 	
 	
-	public Map<Tuple2<String, String>, Long> run() {
+	public Tuple2<Tuple2<String, String>, Long> run() {
 		JavaPairRDD<Tuple2<String, String>, Long> allRoutes =
 				this.flights.mapToPair(flight -> new Tuple2<>(new Tuple2<>(flight.origin, flight.dest) , (long)1));
 
+		JavaPairRDD<Tuple2<String, String>, Long> test = allRoutes.reduceByKey(Long::sum);
+
 
 		JavaPairRDD<Tuple2<String, String>, Long> sorted =  // sort by value
-				allRoutes.mapToPair(x -> x.swap()).sortByKey(false).
-						mapToPair(x -> x.swap());
-		return sorted.countByKey();
+				test.mapToPair(Tuple2::swap).sortByKey(false).
+						mapToPair(Tuple2::swap);
+
+		List<Tuple2<Tuple2<String, String>, Long>> fixed = sorted.take(1);
+		return fixed.get(0);
 	}
 
 }
